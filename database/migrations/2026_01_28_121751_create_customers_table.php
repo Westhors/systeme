f5e80a5ac11832f2466a68f157c762b567030cf6
ahmed abdullah <abdallah@pyramidsfreight.com>
2026-02-09 11:26:27 +0200

    email

--- database/migrations/2026_01_28_121751_create_customers_table.php
+++ database/migrations/2026_01_28_121751_create_customers_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
             $table->string('name')->nullable();
+            $table->string('email')->nullable();
             $table->string('address')->nullable();
             $table->string('phone')->nullable();
             $table->integer('point')->nullable();
