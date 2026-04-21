4726cd438751d906ccf501bedd95a1fb0dd7b456
ahmed abdullah <abdallah@pyramidsfreight.com>
2026-04-21 11:17:58 +0200

    transaction

--- app/Imports/ProductImport.php
+++ app/Imports/ProductImport.php
@@ -17,19 +17,21 @@ class ProductImport implements ToCollection, WithHeadingRow
             foreach ($rows as $row) {
 
                 $stock = (int) ($row['stock'] ?? 0);
-                $cost  = (float) ($row['cost'] ?? 0);
-                $price = (float) ($row['price'] ?? 0);
+                $cost  = (float) str_replace(',', '', $row['cost'] ?? 0);
+                $price = (float) str_replace(',', '', $row['price'] ?? 0);
+
+                $barcode = !empty($row['barcode'])
+                    ? trim($row['barcode'])
+                    : null;
 
                 Product::create([
                     'name'       => $row['name'],
                     'sku'        => $row['sku'],
                     'price'      => $price,
-                    'barcode'    => (string) $row['barcode'],
+                    'barcode'    => $barcode,
                     'stock'      => $stock,
                     'beginning_balance' => 1,
                     'cost'       => $cost,
-                    'total'      => $stock * $cost,
-                    'date'       => now(),
                 ]);
             }
 
