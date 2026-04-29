54c58be0546de547723f5646a3a31d5a373d225a
ahmed abdullah <abdallah@pyramidsfreight.com>
2026-04-29 10:41:47 +0300

    transaction

--- app/Imports/ProductImport.php
+++ app/Imports/ProductImport.php
@@ -3,32 +3,46 @@
 namespace App\Imports;
 
 use App\Models\Product;
+use App\Models\Warehouse;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use Maatwebsite\Excel\Concerns\ToCollection;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
-use Illuminate\Support\Facades\DB;
 
 class ProductImport implements ToCollection, WithHeadingRow
 {
     public function collection(Collection $rows)
     {
         DB::transaction(function () use ($rows) {
-
+            $mainWarehouse = Warehouse::where('main_branch', true)->first();
+            if (!$mainWarehouse) {
+                throw new \Exception('Main warehouse not found');
+            }
             foreach ($rows as $row) {
 
-                Product::updateOrCreate(
+                $stock = (int) ($row['stock'] ?? 0);
+                $cost  = (float) str_replace(',', '', $row['cost'] ?? 0);
+
+                $product = Product::updateOrCreate(
                     ['sku' => trim($row['sku'])],
                     [
                         'name'       => trim($row['name']),
                         'price'      => (float) str_replace(',', '', $row['price'] ?? 0),
                         'barcode'    => !empty($row['barcode']) ? trim($row['barcode']) : null,
-                        'stock'      => (int) ($row['stock'] ?? 0),
+                        'stock'      => $stock,
                         'beginning_balance' => 1,
-                        'cost'       => (float) str_replace(',', '', $row['cost'] ?? 0),
+                        'cost'       => $cost,
                     ]
                 );
-            }
 
+                // اربط المنتج بالمخزن الرئيسي
+                $product->warehouses()->syncWithoutDetaching([
+                    $mainWarehouse->id => [
+                        'stock' => $stock,
+                        'cost'  => $cost,
+                    ]
+                ]);
+            }
         });
     }
 }
